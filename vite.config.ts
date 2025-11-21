3c7218ecbbf1b45def446ecf44abdd44d81ed5d8
krims31 <vegor405@gmail.com>
2025-11-21 17:26:18 +0300

    fix bugs

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  root: "src",
+  build: {
+    outDir: "../dist-react",
+    emptyOutDir: true,
+  },
+  server: {
+    port: 3000,
+  },
+});
